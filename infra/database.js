8690e6697eea4e60e27632d5be888261241971e7
Yoko Lucila Takano <takano_yokolucila@outlook.com>
2023-12-27 20:38:45 -0400

    adiciona o arquivo `jsconfig.json`

--- infra/database.js
+++ infra/database.js
@@ -5,11 +5,10 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSGRES_DB,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  await client.query(queryObject);
   const result = await client.query(queryObject);
   await client.end();
   return result;
