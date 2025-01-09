3ebaa2b88de52b749affb78cf43f9fb426f8522c
Yoko Silva <yltes.eng@uea.edu.br>
2025-01-08 22:03:00 -0400

    add /api/v1/migrations

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const dotnev = require("dotenv");
+dotnev.config({
+  path: ".env.development",
+});
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: ".",
+});
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+module.exports = jestConfig;
